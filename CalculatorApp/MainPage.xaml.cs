d6ae2e91f4726d54b8f37f8de78dce9759840bdf
Amudha <amudha@Amudhas-MacBook-Pro.local>
2023-06-09 22:04:19 -0400

    Initial commit

---
+++ CalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+namespace CalculatorApp;
+
+public partial class MainPage : ContentPage
+{
+	private string current;
+	private bool b;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	void OnClearClicked(object sender, EventArgs e)
+	{
+		label1.Text = "";
+		current = "";
+		b = true;
+    }
+
+    void OnClicked(object sender, EventArgs e)
+	{
+		if (b == true)
+		{
+            current += (sender as Button).Text;
+            label1.Text = current;
+			if (current.Length >= 8)
+			{
+				b = false;
+			}
+        }
+		
+    }
+
+	void calculate(object sender, EventArgs e)
+	{
+        String formattedCalculation = current.ToString().Replace("x", "*");
+
+        try
+		{
+            label1.Text = new DataTable().Compute(formattedCalculation, null).ToString();
+            current = label1.Text;
+        }
+		catch (Exception)
+		{
+            label1.Text = "";
+            current = "";
+			b = true;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # My-Calculator-App
+This app works as a basic five function calculator.
+
+## Dependencies
+using System.Data
+
+## Installation
+Git clone
+
+Open project in Visual Studio
+
+Run the project in the active run configuration.
+
+## Credits
+https://github.com/dotnet/maui-samples/tree/main/7.0/Apps/Calculator
+
+https://www.makeuseof.com/windows-forms-calculator-app-build/
+
+## To Do
+Adjust label character length to not go past what's visible on the screen.
+
+## License
+GNU GENERAL PUBLIC LICENSE
